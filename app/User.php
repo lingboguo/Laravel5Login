6290983a318446c0a1e762f3d5c59498d8896a36
Lingbo <guolingbo1991@gmail.com>
2016-01-12 03:04:01 -0500

    Set up database, add additional field "session_id" in users table.

--- app/User.php
+++ app/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'session_id'
     ];
 
     /**
@@ -21,6 +21,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token', 'session_id'
     ];
 }
