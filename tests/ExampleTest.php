e4628719b3572531b8adffcd92c08ff0b70481d8
Lingbo <guolingbo1991@gmail.com>
2016-01-13 23:09:25 -0500

    test login

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -8,62 +8,36 @@ use Illuminate\Support\Facades\Auth;
 
 class ExampleTest extends TestCase
 {
-    //use DatabaseMigrations;
     /**
-     * A basic functional test example.
-     *
-     * @return void
-     */
-    /*public function testBasicExample()
-    {
-        $this->visit('http://laratest.app/register')
-            ->type('Lingbo', 'name')
-            ->type('lguo@greenedu.com', 'email')
-            ->type('123123', 'password')
-            ->type('123123', 'password_confirmation')
-            ->press('Register')
-            ->seePageIs('http://laratest.app/home');
-    }*/
-
-    /*private $driver1;
-    private $driver2;
-
-    protected function setUp()
-    {
-        $this->driver1 = $this->createDriver();
-
-        $this->driver2 = $this->createDriver();
-    }
-
-    protected function createDriver()
-    {
-        $driver = new PHPUnit_Extensions_Selenium2TestCase();
-        $driver->setBrowser('firefox');
-        $driver->setBrowserUrl('http://www.example.com/');
-        $driver->start();
-
-        return $driver;
-    }*/
-    private $auth = null;
-
-    /**
+     * @group login
      * @return $this
      */
     public function testLoginExample()
     {
+        $user_1 = App\User::where('email', 'guolingbo1991@gmail.com')->first();
+
+        if(is_null($user_1)){
+            $this->visit('http://laratest.app/register')
+                ->type('lingbo', 'name')
+                ->type('guolingbo1991@gmail.com', 'email')
+                ->type('123123', 'password')
+                ->type('123123', 'password_confirmation')
+                ->press('Register')
+                ->seePageIs('http://laratest.app/home');
+        }
+
+        //Start another session(login another browser)
         $response = $this->call('POST', '/login', [
             'email' => 'guolingbo1991@gmail.com',
             'password' => '123123'
         ]);
-        /*$user_1 = App\User::first();
-        Auth::login($user_1);*/
-        $user_1 = App\User::first();
-        $user_2 = Auth::user();
+
+        $user_2 = App\User::where('email', 'guolingbo1991@gmail.com')->first();
+
         var_dump($user_1->session_id);
-        var_dump(Auth::check());
         var_dump($user_2->session_id);
-        var_dump(Session::getHandler()->read($user_1->session_id));
-        var_dump(Session::getHandler()->read($user_2->session_id));
-        //$this->assertEquals(200, $response->getStatusCode());
+
+        $this->assertEquals("",Session::getHandler()->read($user_1->session_id));
+        $this->assertNotEquals("", Session::getHandler()->read($user_2->session_id));
     }
 }
